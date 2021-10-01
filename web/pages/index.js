c5e079f5a1950e0cb314c70126ed4a53f684a3ad
Sean Kramer <seandominickramer@gmail.com>
2021-10-01 13:37:23 -0400

    Add list of posts mapped on home page

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,10 +1,31 @@
-const Index = () => {
-  
-  return (
-    <div>
-      <p>Blog!</p>
-    </div>
-  )
+import Link from 'next/link'
+import groq from 'groq'
+import client from '../client'
+
+const Index = (props) => {
+    const { posts = [] } = props
+    return (
+      <div>
+        <h1>Welcome to a blog!</h1>
+        {posts.map(
+          ({ _id, title = '', slug = '', _updatedAt = '' }) =>
+            slug && (
+              <li key={_id}>
+                <Link href="/post/[slug]" as={`/post/${slug.current}`}>
+                  <a>{title}</a>
+                </Link>{' '}
+                ({new Date(_updatedAt).toDateString()})
+              </li>
+            )
+        )}
+      </div>
+    )
 }
 
-export default Index;
+Index.getInitialProps = async () => ({
+    posts: await client.fetch(groq`
+      *[_type == "post"]|order(publishedAt desc)
+    `)
+})
+
+export default Index
--- web/pages/post/[slug].js
+++ web/pages/post/[slug].js
@@ -1,12 +1,47 @@
+import groq from 'groq'
+import imageUrlBuilder from '@sanity/image-url'
+import BlockContent from '@sanity/block-content-to-react'
 import client from '../../client'
+
+function urlFor (source) {
+  return imageUrlBuilder(client).image(source)
+}
 
 const Post = (props) => {
 
   console.log(JSON.stringify(props));
-  
+  const { 
+    title = 'Missing title', 
+    name = 'Missing name', 
+    categories, 
+    authorImage, 
+    body = []
+  } = props
   return (
     <article>
-      <h1>{props.slug.current}</h1>
+      <h3>Blog Page</h3>
+      <h1>{title}</h1>
+      <span>By {name}</span>
+      {categories && (
+        <ul>
+          Posted in
+          {categories.map(category => <li key={category}>{category}</li>)}
+        </ul>
+      )}
+            {authorImage && (
+        <div>
+          <img
+            src={urlFor(authorImage)
+              .width(100)
+              .url()}
+          />
+        </div>
+      )}
+            <BlockContent
+        blocks={body}
+        imageOptions={{ w: 320, h: 240, fit: 'max' }}
+        {...client.config()}
+      />
     </article>
     
   )
@@ -14,12 +49,19 @@ const Post = (props) => {
   
 }
 
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  "authorImage": author->image,
+  body
+}`
+
 Post.getInitialProps = async function(context) {
   // It's important to default the slug so that it doesn't return "undefined"
+  
   const { slug = "" } = context.query
-  return await client.fetch(`
-    *[_type == "post" && slug.current == $slug][0]
-  `, { slug })
+  return await client.fetch(query, { slug })
 }
 
 export default Post
